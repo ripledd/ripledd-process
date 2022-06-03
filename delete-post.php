54ed3d9dcaf99b990b7351f0bea5567181afdcb3
SpeecherTeam <105486423+SpeecherTeam@users.noreply.github.com>
2022-06-03 17:31:01 +0300

    Update delete-post.php

--- delete-post.php
+++ delete-post.php
@@ -4,8 +4,8 @@ session_start();
 
 if (($_POST['del_post_id']!="")) {
 
-  $secure_id = $_SESSION['secure_id'];
-  $sql = "SELECT * FROM users WHERE secure_id = '$secure_id'";
+  $secure_id = $_SESSION['---'];
+  $sql = "SELECT * FROM --- WHERE --- = '$secure_id'";
     $rs = mysqli_query($dbconn, $sql);
       $fetchRow = mysqli_fetch_assoc($rs);
         $post_id = $_POST['del_post_id'];
@@ -13,19 +13,19 @@ if (($_POST['del_post_id']!="")) {
             exit;
           }
   // get data from post
-  $sql = "SELECT * FROM post_data WHERE id='$post_id'";
+  $sql = "SELECT * FROM --- WHERE ---='$post_id'";
     $post_data_rs = mysqli_query($dbconn, $sql);
       $post_data_row = mysqli_fetch_assoc($post_data_rs);
-        $post_author = $post_data_row['user_id'];
-         $file = $post_data_row['file'];
-          $poster = $post_data_row['poster'];
+        $post_author = $post_data_row['---'];
+         $file = $post_data_row['---'];
+          $poster = $post_data_row['---'];
 
         if ($post_author != "$secure_id") {
             exit;
         }else {
-         $sql = "DELETE FROM post_data WHERE id='$post_id'";
+         $sql = "DELETE FROM --- WHERE --- ='$post_id'";
            $results = mysqli_query($dbconn, $sql);
-            $sql = "DELETE FROM comments_data WHERE post_id='$post_id'";
+            $sql = "DELETE FROM --- WHERE --- ='$post_id'";
              $results = mysqli_query($dbconn, $sql);
               if ($file != '') {
                 unlink("C:/path/to/file/$file");
